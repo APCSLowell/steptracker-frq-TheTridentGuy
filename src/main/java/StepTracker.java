3ec94267c9b5e946ee82d03949f70d1b5904c3cf
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-04-25 23:39:01 -0700

    i must say im a little dissapointed in my on paper performance

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -2,4 +2,28 @@ import java.util.ArrayList;
 public class StepTracker
 {
  /* to be implemented here */
+    private final int activeReq;
+    private int totalSteps = 0;
+    private int totalActiveDays = 0;
+    private int totalDays = 0;
+    public StepTracker(int num)
+    {
+        activeReq = num;
+    }
+    public void addDailySteps(int num){
+        totalSteps += num;
+        totalDays++;
+        if(num >= activeReq){
+            totalActiveDays++;
+        }
+    }
+    public int activeDays(){
+        return totalActiveDays;
+    }
+    public double averageSteps(){
+        if(totalDays == 0){
+            return 0;
+        }
+        return totalSteps / (double) totalDays;
+    }
 } 
